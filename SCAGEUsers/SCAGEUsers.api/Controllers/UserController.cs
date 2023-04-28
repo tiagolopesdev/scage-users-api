8326998d87afddec754318efe7dc4565568d9bf0
Tiago Lopes <saxtiago14@gmail.com>
2023-04-27 21:12:17 -0300

    Feat: controllers created

---
+++ SCAGEUsers/SCAGEUsers.api/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SCAGEUsers.Application.DTO;
+
+namespace SCAGEUsers.api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<UsersDto>> GetAllUsers()
+        {
+            return Ok("Tudo ok");
+        }
+    }
+}
