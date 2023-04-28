ffd37cafdf18b923899a778b065d15bcd77c9f7b
Tiago Lopes <saxtiago14@gmail.com>
2023-04-27 21:15:22 -0300

    Feat: dto to return user properties

---
+++ SCAGEUsers/SCAGEUsers.Application/DTO/UsersDto.cs
@@ -0,0 +1,12 @@
+﻿
+using SCAGEUsers.Application.VO;
+
+namespace SCAGEUsers.Application.DTO
+{
+    public class UsersDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Sex Sex { get; set; }
+    }
+}
---
+++ SCAGEUsers/SCAGEUsers.Application/VO/Sex.cs
@@ -0,0 +1,9 @@
+﻿
+namespace SCAGEUsers.Application.VO
+{
+    public enum Sex
+    {        
+        M = 0,
+        F = 1
+    }
+}
